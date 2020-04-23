ce863a0f6dd04874ea366fc3436f5e3c6370f06f
Mark Basham <mark.basham@diamond.ac.uk>
2020-04-23 23:21:47 +0100

    Extraction of basic functions to utils.js

---
+++ src/utils.js
@@ -0,0 +1,7 @@
+function httpGet(theUrl) {
+  var xmlHttp = null;
+  xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", theUrl, false );
+  xmlHttp.send( null );
+  return xmlHttp.responseText;
+}
