5daa7f7eee953aeb70938f81e0a782cbe38c88e2
Mark Basham <mark.basham@diamond.ac.uk>
2020-04-24 22:10:28 +0100

    pull out loadig and rolling funciton

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,15 @@ function httpGet(theUrl) {
   xmlHttp.send( null );
   return xmlHttp.responseText;
 }
+
+function get_value_from_table(table, lookup='0') {
+  var data_string = httpGet("../json/"+table+".json")
+  var data = JSON.parse(data_string);
+  var index = 'D'+lookup;
+  switch(data.select) {
+    case "D66":
+      index = ('R' + Math.floor(Math.random() * 6 + 1))+ Math.floor(Math.random() * 6 + 1);
+      break;
+  }
+  return data.data[index]
+}
